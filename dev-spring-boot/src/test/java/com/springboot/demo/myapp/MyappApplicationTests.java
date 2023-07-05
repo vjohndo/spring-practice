68b6d7d3ad490c9df44e5062749980264b866125
vjohndo <v.john.do@outlook.com>
2023-07-05 23:13:29 +1000

    spring initialiszr test

---
+++ dev-spring-boot/src/test/java/com/springboot/demo/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
